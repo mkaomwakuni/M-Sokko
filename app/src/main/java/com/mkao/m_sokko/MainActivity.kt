5d5138e1a1579c6d7e96400d21cd6a4130bcd056
mkao <mkaoibrahim@gmail.com>
2022-07-31 11:23:57 +0300

    Colors/Fixed Missing NavController

--- app/src/main/java/com/mkao/m_sokko/MainActivity.kt
+++ app/src/main/java/com/mkao/m_sokko/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.mkao.m_sokko
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mkao.m_sokko.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,12 +21,17 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navController by lazy {
+            val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+
+            navHostFragment.navController
+        }
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_product, R.id.navigation_dashboard, R.id.navigation_checkout
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
