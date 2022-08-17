7c7602d3d236a808bb884c6064b62e7ccce356b4
mkao <mkaoibrahim@gmail.com>
2022-08-17 22:31:32 +0300

    Minor Code/Color fix causing errors

--- app/src/main/java/com/mkao/m_sokko/MainActivity.kt
+++ app/src/main/java/com/mkao/m_sokko/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.mkao.m_sokko
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,9 +14,12 @@ import com.mkao.m_sokko.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val sokoViewModel:SokoViewModel by viewModels() //viewModel Initialised
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getSupportActionBar()?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        getSupportActionBar()?.setCustomView(R.layout.actionbarcustom)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -24,7 +29,6 @@ class MainActivity : AppCompatActivity() {
         val navController by lazy {
             val navHostFragment = supportFragmentManager
                 .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
-
             navHostFragment.navController
         }
         // Passing each menu ID as a set of Ids because each
@@ -36,5 +40,11 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val carrots = ProductItem(R.drawable.carrot,"Carrorts",20.00)
+        val sukumaiki = ProductItem(R.drawable.sukumaiki,"Sukumaiki",10.0)
+        val grapes = ProductItem(R.drawable.strawberry,"Grapes",10.00)
+        val items = listOf(carrots,sukumaiki,grapes)
+        sokoViewModel.products.value =items
     }
 }
